516e55dfb4bc9dd655c2a21c9faa252b9669fa07
Markey Tan <methawee.tan@gmail.com>
2018-05-12 17:31:03 +0800

    FMC

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,3 +3,15 @@ class Api::V1::ItemsController < Api::V1::BaseController
       @items = Item.all
     end
   end
+
+    def show
+      @item = Item.find(params[:id])
+    end
+
+
+    def destroy
+      @restaurant.destroy
+      head :no_content
+      # No need to create a `destroy.json.jbuilder` view
+    end
+end
